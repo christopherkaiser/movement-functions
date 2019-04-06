2a7a2ec90d627683d945f13698839731cdad3bd8
Christopher Kaiser <chris.a.kaiser.7@gmail.com>
2019-04-06 16:50:49 -0500

    modifyed testComponent to handle keydowns and dispatch the move function to redux

--- src/components/testComponent.tsx
+++ src/components/testComponent.tsx
@@ -1,13 +1,51 @@
 import * as React from "react";
 import {AvatarComponentProps} from "../containers/AvatarContainer";
 import logo from './logo.svg';
+import {Vector} from "../store/avatar/types";
 
-const test: React.SFC<AvatarComponentProps> = ({xPos, yPos, move}) => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-  </div>
-);
+const stylings = (xPos: number, yPos: number) => ({
+  position: "absolute", 
+  top: yPos,
+  left: xPos
+} as React.CSSProperties);
+
+const handleKeyDown = (e: KeyboardEvent, dispatch: (v: Vector) => void) => {
+  console.log("test" + e.key);
+  switch(e.key){
+    case "w":
+      dispatch({x: 0, y: -1});
+      break;
+    case "a":
+      dispatch({x: -1, y: 0});
+      break;
+    case "s":
+      dispatch({x: 0, y: 1});
+      break;
+    case "d":
+      dispatch({x: 1, y: 0});
+      break;
+    default:
+      break;
+  }
+};
+
+class test extends React.Component<AvatarComponentProps> {
+  componentDidMount(){
+    document.addEventListener("keydown", e => handleKeyDown(e, this.props.move))
+  }
+    
+  render() { 
+    const {xPos, yPos} = this.props;
+    return(
+      <div className="App" >
+        <header className="App-header">
+          <div style={stylings(xPos, yPos)} >
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default test;
--- src/containers/AvatarContainer.tsx
+++ src/containers/AvatarContainer.tsx
@@ -3,11 +3,12 @@ import {connect} from "react-redux";
 import {AvatarState} from "../store/avatar/types";
 import {AppState} from "../store";
 import {moveAvatar} from "../store/avatar/actions";
+import test from "../components/testComponent";
 
 interface AvatarContainerProps {
   avatarState: AvatarState;
   moveAvatar: typeof moveAvatar;
-  AvatarComponent: React.SFC<AvatarComponentProps>;
+  AvatarComponent: typeof React.Component;
 }
 
 export interface AvatarComponentProps {
